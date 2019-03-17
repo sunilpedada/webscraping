370faecce7d7dbc753ce8877d84efd5fe202f964
sunilpedada <47655864+sunilpedada@users.noreply.github.com>
2019-03-18 00:23:58 +0530

    Update mydata2.py

--- mydata2.py
+++ mydata2.py
@@ -1,9 +1,10 @@
 import json
 with open('mydata1.json','r') as a:
-    b=json.load(a)
-for q in b:
-    for n in q:
-        print(n['dow'])
-    
-
-
+    b=(json.load(a))
+for q,c,d, in b:
+	n=(q['dow'])
+	m=(c['time'])
+	f=d.get('conference-categories-count')
+	o=(f.get('Large'))
+	print({n:[m,o]})
+	
